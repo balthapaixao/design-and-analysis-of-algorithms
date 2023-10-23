79a6da1894dc22827b251da7197b6ccbf388d364
balthapaixao <balthazarpaixao@gmail.com>
2023-10-23 15:41:08 -0300

    fix: changes in flight controller

--- python-codes/assignment-01/12-flight_controller.py
+++ python-codes/assignment-01/12-flight_controller.py
@@ -1,17 +1,3 @@
-"""## Exercício 12
-
-Faça um programa para simular um controlador de voo de um aeroporto. Neste programa o usuário deve ser capaz de realizar as seguintes tarefas:
-
-- Listar o número de aviões esperando para decolar;
-- Autorizar a decolagem do primeiro avião na fila;
-- Adicionar um avião na fila de espera;
-- Listar todos os aviões que estão na lista de espera;
-- Listar as características do primeiro avião da fila;
-
-Considere que uma estrutura de dados do tipo fila seja usada para manipular os dados e que cada avião possui um nome, um identificador, uma origem e um destino. Se quiser coloque mais informações, nº de passageiros, capacidade, modelo, etc.
-"""
-
-
 class Flight:
     def __init__(self, name, id, origin, destination):
         self.name = name
