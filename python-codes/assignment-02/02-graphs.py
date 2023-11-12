d022e3d9d1b367e741b2c94b064d23e031d16801
balthapaixao <balthazarpaixao@gmail.com>
2023-11-11 21:58:50 -0300

    feat graph 02

---
+++ python-codes/assignment-02/02-graphs.py
@@ -0,0 +1,36 @@
+"""4  –  Quando  trabalhamos  com  grafos  admitimos  que  algumas  operações  básicas  já  estejam 
+implementadas.  Então  nossa  primeira  tarefa  é  implementar  as  seguintes  operações  básicas 
+tanto para uma representação por matriz de adjacências quanto para lista de adjacências: 
+a) Definição do Grafo 
+b) Adição de Arestas 
+c) Determinação de vizinhos de um dado vértice 
+d) Teste de vizinhança entre par de vértices 
+e) Remoção de arestas """
+
+
+class Graph:
+    def __init__(self) -> None:
+        self.graph = {}
+
+    def add_edge(self, node1: int, node2: int):
+        if node1 not in self.graph:
+            self.graph[node1] = set()
+        if node2 not in self.graph:
+            self.graph[node2] = set()
+
+        self.graph[node1].add(node2)
+        self.graph[node2].add(node1)
+
+    def get_neighbors(self, node: int):
+        return self.graph[node]
+
+    def is_neighbor(self, node1: int, node2: int):
+        return node2 in self.graph[node1]
+
+    def remove_edge(self, node1: int, node2: int):
+        self.graph[node1].remove(node2)
+        self.graph[node2].remove(node1)
+
+    def graph_definition(self, nodes: list, edges: list):
+        self.nodes = nodes
+        self.edges = edges
