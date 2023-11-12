54462366dba0b6f4860e57f0e31290ebac46181e
balthapaixao <balthazarpaixao@gmail.com>
2023-11-12 09:06:55 -0300

    update

--- python-codes/assignment-02/02-graphs.py
+++ python-codes/assignment-02/02-graphs.py
@@ -34,3 +34,35 @@ class Graph:
     def graph_definition(self, nodes: list, edges: list):
         self.nodes = nodes
         self.edges = edges
+
+
+if __name__ == "__main__":
+    nodes = [1, 2, 3, 4, 5]
+    edges = [
+        (1, 1),
+        (1, 2),
+        (2, 1),
+        (2, 3),
+        (3, 3),
+        (3, 4),
+        (4, 3),
+        (4, 4),
+        (5, 4),
+        (5, 5),
+    ]
+    edges = [(1, 2), (1, 3), (3, 4), (3, 4), (4, 5), (5, 5)]
+
+    G = Graph()
+    G.graph_definition(nodes, edges)
+    G.add_edge(1, 2)
+    G.add_edge(1, 3)
+    G.add_edge(3, 4)
+    G.add_edge(3, 4)
+    G.add_edge(4, 5)
+    G.add_edge(5, 5)
+    print(G.graph)
+    print(G.get_neighbors(1))
+    print(G.is_neighbor(1, 2))
+    G.remove_edge(1, 2)
+    print(G.graph)
+    print(G.is_neighbor(1, 2))
